b654f3f459fb18d3dcae2599e5ac891ba0294fb9
MOHAMMED HUZAIF AHMED <104403864+legendhuzaif@users.noreply.github.com>
2023-01-11 13:54:35 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,28 @@
+
+let Employees= [{
+    Name: "Scott",
+    Address: "Hyderabad",
+    Salary: 500000,
+    DateOfJoining: "23-1-2012"
+},
+{
+    Name: "Ram",
+    Address: "Hyderabad",
+    Salary: 300000,
+    DateOfJoining: "23-12-2010"
+},
+{
+    Name: "John",
+    Address: "Bangalore",
+    Salary: 700000,
+    DateOfJoining: "23-12-2010"
+}
+]
+
+for (i=0;i<Employees.length;i++){
+    document.write(Employees[0].Name)
+    document.write(Employees[0].Address)
+    document.write(Employees[0].Salary)
+    document.write(Employees[0].DateOfJoining)
+
+}
---
+++ practice.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'Scott',
+    age: 25,
+    address: {
+      city: 'Hyderabad',
+      state: 'Telangana'
+    }
+  }
+
+var a={name:'john',...person}
+console.log(a)
+
+
